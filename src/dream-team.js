a0de60e3f3ce90b01c8e824e935380008ceaf5a9
Natalia Lankevich <nataliiks@rambler.ru>
2022-04-28 12:52:05 +0300

    feat: add solution task

--- src/dream-team.js
+++ src/dream-team.js
@@ -14,7 +14,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+ // throw new NotImplementedError('Not implemented');
+ 
+  let result = this.Array.filter(item => typeof(item) == "string").map(item => item = item[0]).join("");
+ 
+
   // remove line with error and write your code here
 }
 
