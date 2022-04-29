b9624b01bf4010b74be2b1721a8d6d8a35aea0d0
Natalia Lankevich <nataliiks@rambler.ru>
2022-04-30 00:08:55 +0300

    feat: add solution task Extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options) {
+    
+  let additionToString = [];
+  if ((options.addition !== undefined)) {
+  for (let i = 0; i < (options.additionRepeatTimes || 1); i++) {
+    additionToString.push(String(options.addition));
+  }
+  }
+  
+  let separatorAddition = options.additionSeparator || "|";
+
+  let additionWithSeparator = additionToString.join(`${separatorAddition}`)
+   
+  let string =[];
+
+  for (let i = 0; i < (options.repeatTimes || 1); i++) {
+    if(additionToString.length > 0) {
+      string.push (String(str) + additionWithSeparator);
+    } else {
+      string.push (String(str))
+    }
+  }
+  
+  let separatorString = options.separator || "+";
+  let res = string.join(`${separatorString}`)
+  return res;
+ 
+  
 }
 
 module.exports = {
